c056327ca2ab9efc3edce4043bf329afe0414156
happysmaran <smaranvallabhaneni@gmail.com>
2026-01-18 13:16:35 +0530

    Sprint 3 - early

--- run-qemu.sh
+++ run-qemu.sh
@@ -4,7 +4,7 @@ BINARIES_DIR="${0%/*}/"
 # shellcheck disable=SC2164
 cd "${BINARIES_DIR}"
 
-mode_serial=false
+mode_serial=true
 mode_sys_qemu=false
 while [ "$1" ]; do
     case "$1" in
@@ -25,4 +25,10 @@ if ! ${mode_sys_qemu}; then
     export PATH="/home/smaran/Desktop/OuroborOS/buildroot/output/host/bin:${PATH}"
 fi
 
-exec qemu-system-x86_64 -M pc -kernel "buildroot/output/images/bzImage" -drive file="buildroot/output/images/rootfs.ext2",if=virtio,format=raw -append "rootwait root=/dev/vda console=tty1 console=ttyS0"  -net nic,model=virtio -net user  ${EXTRA_ARGS} "$@"
+exec qemu-system-x86_64 -M pc \
+    -kernel "buildroot/output/images/bzImage" \
+    -drive file="buildroot/output/images/rootfs.ext2",if=virtio,format=raw \
+    -append "rootwait root=/dev/vda console=ttyS0 lpj=10000000 clocksource=pit notsc noapic" \
+    -net nic,model=virtio -net user,hostfwd=tcp::9000-:9000 \
+    ${EXTRA_ARGS} -accel tcg "$@" \
+    -rtc base=utc,clock=vm -icount shift=auto,sleep=on
